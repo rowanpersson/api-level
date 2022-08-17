e25628e2b4f470ec8861c4cf2d94a1ebb099f62a
Rowan Persson <RPersso3@its.jnj.com>
2022-08-17 16:02:23 -0500

    moving virtual env to project root and adding attempt to connect to local SSMS DB

--- app/app.py
+++ app/app.py
@@ -3,6 +3,25 @@ import pyodbc
 
 app = Flask(__name__)
 
+def connection():
+    s = 'A-PF1F43HN\SQLEXPRESS'
+    d = 'App_Data' 
+    u = 'sa'
+    p = 'password123!'
+    cstr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+s+';DATABASE='+d+';UID='+u+';PWD='+p
+    conn = pyodbc.connect(cstr)
+    return conn
+
 @app.route('/')
-def index():
-    return render_template('base.html')
+def main():
+    customers = []
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM dbo.customers")
+    for row in cursor.fetchall():
+        customers.append({"customer_id": row[0], "customer_name": row[1]})
+    conn.close()
+    return render_template("base.html", customers = customers)
+
+if(__name__ == "__main__"):
+    app.run()
